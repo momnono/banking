2e308832c1079fc25e98456593973303c4ede251
mo <alshmoha@shernet.sheridancollege.ca>
2023-04-02 23:29:29 -0400

    added main menu

--- Bnakingapp.py
+++ Bnakingapp.py
@@ -80,10 +80,25 @@ class ChequingAccount(Account):
 
 class Program:
     def __init__(self):
-        pass
+        self.bank = Bank("Your Bank Name")
 
     def show_main_menu(self):
-        pass
+        while True:
+            print("\nMain Menu:")
+            print("1. Open Account")
+            print("2. Select Account")
+            print("3. Exit")
+            choice = input("Enter your choice: ")
+
+            if choice == '1':
+                self.open_account()
+            elif choice == '2':
+                self.select_account()
+            elif choice == '3':
+                print("Goodbye!")
+                break
+            else:
+                print("Invalid choice. Please try again.")
 
     def open_account(self):
         pass
